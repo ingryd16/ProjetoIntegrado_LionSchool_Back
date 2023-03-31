924327540da7cfad7a9eeaf30a25789393f4a4c4
Gustavo <gustavo326projetos@gmail.com>
2023-03-31 18:05:56 -0300

    Finalizando Back

--- modulo/json/cursos.js
+++ modulo/json/cursos.js
@@ -2,13 +2,13 @@ var cursos = [
     {
         "nome"  :   "001 - Técnico em Desenvolvimento de Sistemas",
         "sigla" :   "DS",
-        "icone" :   "https://image.shutterstock.com/image-vector/api-interface-vector-icon-600w-659203513.jpg",
+        "icone" :   "./img/DS.png",
         "carga" :   "1200",
     },
     {
         "nome"  :   "002 - Técnico em Redes de Computadores",
         "sigla" :   "RDS",
-        "icone" :   "https://img.icons8.com/ultraviolet/344/thin-client.png",
+        "icone" :   "./img/RDS.png",
         "carga" :   "1200"
     }
 ];
