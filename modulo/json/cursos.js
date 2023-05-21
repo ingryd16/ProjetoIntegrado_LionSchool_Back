81dc4f979b781bdcfcc83b09ec2c3804c42c7f90
Gustavo Henrique <Gustavo326projetos@gmail.com>
2023-05-21 19:42:15 -0300

    Atualizando cursos

--- modulo/json/cursos.js
+++ modulo/json/cursos.js
@@ -1,12 +1,12 @@
 var cursos = [
     {
-        "nome"  :   "001 - Técnico em Desenvolvimento de Sistemas",
+        "nome"  :   "001 - Systems Development Technician",
         "sigla" :   "DS",
         "icone" :   "https://github.com/GustavoHenriqueProjects/ProjetoIntegrado_LionSchool_Front_New/blob/main/img/DS.png?raw=true",
         "carga" :   "1200",
     },
     {
-        "nome"  :   "002 - Técnico em Redes de Computadores",
+        "nome"  :   "002 - Computer Network Technician",
         "sigla" :   "RDS",
         "icone" :   "https://github.com/GustavoHenriqueProjects/ProjetoIntegrado_LionSchool_Front_New/blob/main/img/RDS.png?raw=true",
         "carga" :   "1200"
